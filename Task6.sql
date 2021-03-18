158081df5448e5492130d4c69c2f36df67bc8fe8
Kaustav Roy <kaustavr25@gmail.com>
2021-03-18 13:03:18 +0530

    Average amount of debt across indicators
    So, it was China. A more in-depth breakdown of China's debts can be found here.
    
    We now have a brief overview of the dataset and a few of its summary statistics. We already have an idea of the different debt indicators in which the countries owe their debts. We can dig even further to find out on average how much debt a country owes? This will give us a better sense of the distribution of the amount of debt across different indicators.

---
+++ Task6.sql
@@ -0,0 +1,9 @@
+%%sql
+SELECT 
+    indicator_code AS debt_indicator,
+    indicator_name,
+    AVG(debt) AS average_debt
+FROM international_debt
+GROUP BY debt_indicator, indicator_name
+ORDER BY average_debt DESC
+LIMIT 10;
